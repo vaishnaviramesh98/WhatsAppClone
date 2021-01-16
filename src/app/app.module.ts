6d286f46794ab6f0d155aee13ed05420ef25ea3f
Vaishnavi <vaishnaviramesh58@gmail.com>
2021-01-15 21:02:00 -0800

    first commit: Component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainContainerComponent } from './components/main-container/main-container.component';
+import { SidebarComponent } from './components/main-container/sidebar/sidebar.component';
+import { SidebarContentComponent } from './components/main-container/sidebar/sidebar-content/sidebar-content.component';
+import { ChatAreaComponent } from './components/main-container/chat-area/chat-area.component';
+import { ChatDefaultPageComponent } from './components/main-container/chat-area/chat-default-page/chat-default-page.component';
+import { ChatRoomComponent } from './components/main-container/chat-area/chat-room/chat-room.component';
+import { LoginComponent } from './components/login/login.component';
+import {MaterialModule} from './shared/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContainerComponent,
+    SidebarComponent,
+    SidebarContentComponent,
+    ChatAreaComponent,
+    ChatDefaultPageComponent,
+    ChatRoomComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
